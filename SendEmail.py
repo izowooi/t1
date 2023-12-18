de4699e92418c7ad7f6ff839acc329ddd68c0e99
jongwoo <izowooi@hotmail.com>
2023-12-18 23:58:38 +0900

    추세 매매 최초 추가

---
+++ SendEmail.py
@@ -0,0 +1,31 @@
+import os
+from dotenv import load_dotenv
+import smtplib
+from email.mime.text import MIMEText
+
+
+def send_email(subject, content, recipient):
+    load_dotenv()
+
+    smtp_server = 'smtp.gmail.com'
+    smtp_port = 587
+    sender_email = 'izowooi85@gmail.com'
+    sender_password = os.getenv('EMAIL_PASSWORD')#비밀번호 가져오기
+
+    # SMTP 서버 연결 및 로그인
+    smtp = smtplib.SMTP(smtp_server, smtp_port)
+    smtp.starttls()
+    smtp.login(sender_email, sender_password)
+
+    # 이메일 메시지 생성
+    msg = MIMEText(content)
+    msg['Subject'] = subject
+    msg['From'] = sender_email
+    msg['To'] = recipient
+
+    # 이메일 전송
+    smtp.sendmail(sender_email, recipient, msg.as_string())
+    smtp.quit()
+
+
+#send_email('sub', 'con', 'izowooi85@gmail.com')
---
+++ readme.md
@@ -0,0 +1,5 @@
+
+
+##설치한 패키지명
+- yfinance
+- python-dotenv
