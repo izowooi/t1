b72380363c0fc68e4136c4a7440e93ec751dffa1
jongwoo <izowooi@hotmail.com>
2023-12-19 19:29:31 +0900

    젠킨스에서 받아온 환경변수 출력하도록 수정

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from SendTradingSignal import send_trading_signal_alert
 
 def main():
     ticker_list_str = os.getenv('ticker_list', 'MSFT')
-
+    dotenv_path = os.getenv('dotenv_path', '.env')
+    print(f'ticker_list_str의 값: {ticker_list_str}')
+    print(f'dotenv_path의 값: {dotenv_path}')
     ticker_list = ticker_list_str.split(',')
 
     # 리스트의 각 요소에 대해 반복문을 실행합니다.
