70cbde19815ee59a5743be84d3a7e6b03b8d3e2d
jongwoo <izowooi@hotmail.com>
2024-01-28 23:40:01 +0900

    네이버 증권 API 연동 추가 ( 국내 ETF )

--- SendTradingSignal.py
+++ SendTradingSignal.py
@@ -2,6 +2,10 @@
 import yfinance as yf
 from SendEmail import send_email
 from SendTelegram import send_telegram
+import requests
+from datetime import datetime
+import pandas as pd
+
 
 def calculate_moving_averages(data, short_window=5, long_window=60):
     """
@@ -14,6 +18,7 @@ def calculate_moving_averages(data, short_window=5, long_window=60):
     data['Short_MA'] = data['Close'].rolling(window=short_window).mean()
     data['Long_MA'] = data['Close'].rolling(window=long_window).mean()
     return data
+
 
 def generate_signals(data):
     """
@@ -30,10 +35,54 @@ def str_to_bool(s):
     return s.lower() in ('true', '1', 't', 'y', 'yes')
 
 
-def send_trading_signal_alert(ticker, dotenv_path='.env', test_buy_signal=False):
+def data_from_yahoo(ticker):
     print(f'매매 신호 확인: {ticker}')
     data = yf.download(ticker, start='2020-01-01')
 
+    return data
+
+#364980
+def data_from_naver(ticker):
+    current_date = datetime.now()
+    end_date = current_date.strftime('%Y%m%d')
+    start_date = '20200101'
+    print(f'매매 신호 확인: {ticker}')
+    print(f'end_date : {end_date}')
+    url = f"https://api.finance.naver.com/siseJson.naver?symbol={ticker}&requestType=1&startTime={start_date}&endTime={end_date}&timeframe=day"
+
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print(f"Error: {response.status_code}")
+        return None
+
+    data = response.text
+
+    data = data.replace('\n', '').replace('\t', '').replace(' ', '')
+    data = data.replace('날짜', 'Date')
+    data = data.replace('시가', 'Open')
+    data = data.replace('고가', 'High')
+    data = data.replace('저가', 'Low')
+    data = data.replace('종가', 'Close')
+    data = data.replace('거래량', 'Volume')
+    data = data.replace('외국인소진율', 'Foreigner')
+
+    data = eval(data)
+
+    data = pd.DataFrame(data[1:], columns=data[0])
+
+    return data
+
+
+def send_trading_signal_alert(ticker, dotenv_path='.env', is_yahoo=True, test_buy_signal=False):
+    if is_yahoo:
+        data = data_from_yahoo(ticker)
+    else:
+        data = data_from_naver(ticker)
+
+    if data is None:
+        Exception('데이터를 가져오지 못했습니다.')
+        return
     # 이동 평균 계산
     data = calculate_moving_averages(data, short_window=5, long_window=60)
 
--- main.py
+++ main.py
@@ -11,14 +11,16 @@ def main():
     ticker_list_str = sys.argv[1] if len(sys.argv) > 1 else 'MSFT'
     dotenv_path = sys.argv[2] if len(sys.argv) > 2 else '.env'
     test_buy_signal = str_to_bool(sys.argv[3]) if len(sys.argv) > 3 else False
+    is_yahoo = str_to_bool(sys.argv[4]) if len(sys.argv) > 4 else False
 
     print(f'ticker_list_str : {ticker_list_str}')
     print(f'dotenv_path : {dotenv_path}')
     print(f'test_buy_signal : {test_buy_signal}')
+    print(f'is_yahoo : {is_yahoo}')
     ticker_list = ticker_list_str.split(',')
 
     for ticker in ticker_list:
-        send_trading_signal_alert(ticker, dotenv_path, test_buy_signal)
+        send_trading_signal_alert(ticker, dotenv_path, is_yahoo, test_buy_signal)
 
 
 main()
